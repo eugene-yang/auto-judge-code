fa41897b347aeeac0e94432a3ec469c6719f56a5
Maik Fröbe <maik.froebe@uni-jena.de>
2025-12-10 11:19:46 +0100

    add more datasets

--- tests/test_simplistic_access_to_runs.py
+++ tests/test_simplistic_access_to_runs.py
@@ -87,6 +87,65 @@ class TestSimplisticAccessToRuns(unittest.TestCase):
             self.assertEqual(expected_metadata, [i["metadata"] for i in actual])
             self.assertEqual(expected_paths, [Path(i["path"]).name for i in actual])
 
+    def test_trec25_spot_check_runs_with_narrative_and_topic_id_as_int(self):
+        expected_metadata = [
+            {'team_id': 'my_fantastic_team', 'run_id': 'my_best_run_01', 'topic_id': '101', 'narrative_id': '101'},
+            {'team_id': 'my_fantastic_team', 'run_id': 'my_best_run_02', 'topic_id': '101', 'narrative_id': '101'},
+            {'team_id': 'my_fantastic_team', 'run_id': 'my_best_run_02_citations', 'topic_id': '101', 'narrative_id': '101'}
+        ]
+        expected_paths = [
+            'my_best_run_01',
+            'my_best_run_02',
+            'my_best_run_02_with_citations'
+        ]
+
+        with TemporaryDirectory() as d:
+            for f in glob(str(TREC_25_DATA / "spot-check-dataset" / "runs") + "/*" ):
+                f = Path(f)
+                txt = []
+                for l in f.read_text().split("\n"):
+                    if not l:
+                        continue
+                    l = json.loads(l)
+                    l["metadata"]["topic_id"] = int(l["metadata"]["topic_id"])
+                    l["metadata"]["narrative_id"] = l["metadata"]["topic_id"]
+                    txt.append(json.dumps(l))
+                (Path(d) / f.name).write_text("\n".join(txt))
+
+            actual = load_runs_failsave(Path(d))
+            self.assertEqual(3, len(actual))
+            self.assertEqual(expected_metadata, [i["metadata"] for i in actual])
+            self.assertEqual(expected_paths, [Path(i["path"]).name for i in actual])
+
+    def test_trec25_spot_check_runs_with_qa_id_as_int(self):
+        expected_metadata = [
+            {'team_id': 'my_fantastic_team', 'run_id': 'my_best_run_01', 'topic_id': '101', 'narrative_id': '101'},
+            {'team_id': 'my_fantastic_team', 'run_id': 'my_best_run_02', 'topic_id': '101', 'narrative_id': '101'},
+            {'team_id': 'my_fantastic_team', 'run_id': 'my_best_run_02_citations', 'topic_id': '101', 'narrative_id': '101'}
+        ]
+        expected_paths = [
+            'my_best_run_01',
+            'my_best_run_02',
+            'my_best_run_02_with_citations'
+        ]
+
+        with TemporaryDirectory() as d:
+            for f in glob(str(TREC_25_DATA / "spot-check-dataset" / "runs") + "/*" ):
+                f = Path(f)
+                txt = []
+                for l in f.read_text().split("\n"):
+                    if not l:
+                        continue
+                    l = json.loads(l)
+                    l["metadata"]["qa_id"] = int(l["metadata"]["topic_id"])
+                    del l["metadata"]["topic_id"]
+                    txt.append(json.dumps(l))
+                (Path(d) / f.name).write_text("\n".join(txt))
+
+            actual = load_runs_failsave(Path(d))
+            self.assertEqual(3, len(actual))
+            self.assertEqual(expected_metadata, [i["metadata"] for i in actual])
+            self.assertEqual(expected_paths, [Path(i["path"]).name for i in actual])
 
     def test_trec25_spot_check_runs_with_inconsistent_narrative_and_topic_id(self):
         with TemporaryDirectory() as d:
